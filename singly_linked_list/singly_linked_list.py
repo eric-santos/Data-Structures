19be7c91acc15fd7689614fe524657e60c62fc8b
eric santos <eric355402@yahoo.com>
2020-09-30 14:37:27 -0600

    fixed remove_tail error

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -4,7 +4,7 @@ class Node:
         self.next_node = next_node #next node in list
 
     def get_value(self):
-        return self.value
+        return self.get_value
 
     def get_next_node(self):
         return self.get_next_node
@@ -20,7 +20,6 @@ class LinkedList:
 
 
     def add_to_tail(self, value):
-        
         if not self.tail:
             # create new node
             new_tail = Node(value, None)
@@ -67,13 +66,13 @@ class LinkedList:
         if self.tail is None:
             return None
         else:
-            ret_value = self.tail.get_value()
+            ret_value = self.tail.get_value
             # 1 element
             if self.tail is self.head:
                 # set values to None, delete
                 self.head = None
                 self.tail = None
                 # if more than 1 element
-            else: 
+            else:
                 self.tail = self.tail.get_next_node()
                 return ret_value
